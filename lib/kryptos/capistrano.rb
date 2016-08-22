65741a87d4cd5fddec6941ac9444537050ffebc9
wlipa <dojo@masterleep.com>
2016-08-22 15:53:09 -0700

    v2.1.1 - remove some debug logging

--- lib/kryptos/capistrano.rb
+++ lib/kryptos/capistrano.rb
@@ -3,7 +3,6 @@ namespace :kryptos do
   desc 'Deploy key file for kryptos'
   task :deploy_key do
     on roles(:app) do
-      puts "#{fetch(:deploy_to)}/config/kryptos.key"
       upload! 'config/kryptos.key', "#{fetch(:release_path)}/config/kryptos.key"
     end
   end
